b8cfdae39b57990e7a28aa91502300962afa602c
sueidyyag24 <140613264+sueidyyag24@users.noreply.github.com>
2023-08-04 11:46:11 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+//window.alert('Meu Primeiro Alerta')
+//window.confirm('Está gostando do curso?')
+//window.prompt('Qual seu nome?')
+ //var nome = window.prompt('Qual seu nome?')
+ //window.alert('Seja bem vindo ' + nome+ '!')
+  //var n1 = window.prompt('Digite um número')
+  //var n2 = window.prompt('Digite outro número')
+  //var s = n1 + n2
+  //var s = window.alert('A soma dos 2 números é '+ s)
+
+  //var n1 = Number.parseInt(window.prompt('Digite seu primeiro número'))
+  //var n2 = Number.parseInt(window.prompt('Digite outro número'))
+   //var s = n1+n2
+   //var s = window.alert('A soma dos 2 números' + s)
+
+   var n1= Number(window.prompt('Digite um número'))
+   var n2= Number(window.prompt('Digite outro número'))
+   var s = n1+n2
+   var s = window.alert('A soma dos 2 números é' + s)
+
+
